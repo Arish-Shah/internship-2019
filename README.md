1d0cbc21dc52173698b009ff3ea3ab3fac50cf1d
Arish-Shah <arish0246@gmail.com>
2019-09-21 20:00:00 -0600

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # internship-2019
+
+1. [Project 1: Blog](blog)
+
+1. [Project 2](project-2)
+
+1. [Project 3](project-3)
---
+++ blog/precache-manifest.85a2b0785b71f94f4826e884ae3f9a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b5f650ba63529659163c7d8c208304",
+    "url": "/internship-2019/blog/index.html"
+  },
+  {
+    "revision": "8af6914db17d6cd7d212",
+    "url": "/internship-2019/blog/static/css/main.fd03c36d.chunk.css"
+  },
+  {
+    "revision": "c55c6758115e5d516ac6",
+    "url": "/internship-2019/blog/static/js/2.b9137699.chunk.js"
+  },
+  {
+    "revision": "8af6914db17d6cd7d212",
+    "url": "/internship-2019/blog/static/js/main.b569aa25.chunk.js"
+  },
+  {
+    "revision": "a45690b2a3d494680c8c",
+    "url": "/internship-2019/blog/static/js/runtime-main.315aa938.js"
+  },
+  {
+    "revision": "938f154706225c17b45b05b1489d5cda",
+    "url": "/internship-2019/blog/static/media/avatar.938f1547.jpg"
+  }
+]);
